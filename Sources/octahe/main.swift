de8a3f9bf468cacff55921c4664d311568d90711
Kevin Carter <kecarter@redhat.com>
2020-06-07 23:26:21 -0500

    Provide real options and scafolding
    While the application still does nothing, the options created will
    now validate inpute and ensure all provide values from the end user
    are sane, and could result in a real deployment.

--- Sources/octahe/main.swift
+++ Sources/octahe/main.swift
@@ -5,7 +5,8 @@
 //  Created by Kevin Carter on 6/4/20.
 //
 
-// test args made using docs https://github.com/apple/swift-argument-parser/blob/master/Documentation/02%20Arguments%2C%20Options%2C%20and%20Flags.md
+// test args made using docs:
+// https://github.com/apple/swift-argument-parser/blob/master/Documentation/
 
 import Foundation
 
@@ -14,35 +15,92 @@ import ArgumentParser
 struct Octahe: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "Octahe, a utility for deploying OCI compatible applications.",
-        subcommands: [Deploy.self],
+        subcommands: [Deploy.self, UnDeploy.self],
         defaultSubcommand: Deploy.self
     )
     struct Options: ParsableArguments {
-        @Flag(name: [.customLong("booldata"), .customShort("q")], help: "Boolean Option.")
-        var boolOption: Bool
+        // Global options used in with all subcommands.
+        @Option(
+            name: [.customLong("connection-key"), .customShort("k")],
+            help: "Key used to initiate a connection."
+        )
+        var connectionKey: String?
 
-        @Option(name: .shortAndLong, help: "Some integer to print.")
-        var integerOption: Int?
+        @Option(
+            name: .shortAndLong,
+            help: "Limit the total number of concurrent connections per group."
+        )
+        var connectionQuota: Int?
+        
+        @Flag(
+            help: """
+                  Dry run. This option will perform all nessisary introspection, compile an application deployment
+                  plan, and validate connectivity to targets; it will NOT run the compiled application deployment
+                  plan.
+                  """
+        )
+        var dryRun: Bool
+        
+        @Option(
+            name: .shortAndLong,
+            help: "Escalation binary."
+        )
+        var escalate: String?
+        
+        @Option(
+            name: [.customLong("escalation-pw"), .customShort("p")],
+            help: "Passowrd used for privledge escallation."
+        )
+        var escalatePassword: String?
 
-        @Argument(help: "Some positional argument.")
-        var positionalArgs: String
-
-        mutating func run() throws {
-            print(integerOption as Any, boolOption, positionalArgs)
+        @Option(
+            name: .shortAndLong,
+            help: """
+                  Override or set targets. Any specified on the CLI will be"
+                  the only targets used within a given execution.
+                  """
+        )
+        var targets: [String]
+        
+        @Argument(
+            help: "Configuration file(s) used to build an application deployment plan."
+        )
+        var configurationFiles: [String]
+    
+        mutating func validate() throws {
+            guard !configurationFiles.isEmpty else {
+                throw ValidationError(
+                    "Please provide at least one OCI compatible configuration file to parse."
+                )
+            }
         }
     }
 }
 
 extension Octahe {
     struct Deploy: ParsableCommand {
-        static var configuration
-            = CommandConfiguration(abstract: "Run a deployment for a given OCI compatible application.")
+        static var configuration = CommandConfiguration(
+                abstract: "Run a deployment for a given OCI compatible application."
+        )
 
         @OptionGroup()
         var options: Octahe.Options
         
         mutating func run() {
-            print("Hello, World")
+            print(options)
+        }
+    }
+    
+    struct UnDeploy: ParsableCommand {
+        static var configuration = CommandConfiguration(
+                abstract: "Disable a deployment for a given OCI compatible application."
+        )
+
+        @OptionGroup()
+        var options: Octahe.Options
+        
+        mutating func run() {
+            print(options)
         }
     }
 }
