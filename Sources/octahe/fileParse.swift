b64405ffc244ad13fbd28e9b7423bba61cac9394
Kevin Carter <kecarter@redhat.com>
2020-06-10 23:12:12 -0500

    added additional line parsing and formatting
    Signed-off-by: Kevin Carter <kecarter@redhat.com>

--- Sources/octahe/fileParse.swift
+++ Sources/octahe/fileParse.swift
@@ -13,8 +13,9 @@ enum FileParserError: Error {
 }
 
 class FileParser {
-    class func buildRawConfigs(files:Array<String>) throws -> String {
+    class func buildRawConfigs(files:Array<String>) throws -> [(key: String, value: String)] {
         var rawConfigs: [String] = []
+        var configOptions: [(key: String, value: String)] = []
         for file in files {
             do {
                 let configData = try String(contentsOfFile: file)
@@ -23,14 +24,25 @@ class FileParser {
                 throw FileParserError.FileReadFailure(filePath: file)
             }
         }
-        var trimmedList = [String]()
         for rawconfig in rawConfigs {
-            let trimmed = rawconfig.trimmingCharacters(in: .whitespacesAndNewlines)
-            if !trimmed.isEmpty {
-                trimmedList.append(trimmed)
+            let lines = rawconfig.split(whereSeparator: \.isNewline)
+            // TODO(cloudnull): the line parser should handle a multi-line VERB.
+            for line in lines {
+                let trimmed = line.replacingOccurrences(of: "#.*", with: "", options: [.regularExpression])
+                let cleanedLine = trimmed.trimmingCharacters(in: .whitespacesAndNewlines)
+                if !cleanedLine.isEmpty {
+                    let verbArray = cleanedLine.split(separator: " ", maxSplits: 1).map(String.init)
+                    do {
+                        let data = verbArray[1].data(using: .utf8)!
+                        let json = try JSONSerialization.jsonObject(with: data, options : .allowFragments) as! Array<String>
+                        let joined = json.joined(separator: " ")
+                        configOptions.append((key: verbArray[0], value: String(joined)))
+                    } catch {
+                        configOptions.append((key: verbArray[0], value: verbArray[1]))
+                    }
+                }
             }
         }
-        let joined = trimmedList.joined(separator: "\n")
-        return joined
+        return configOptions
     }
 }
--- Sources/octahe/router.swift
+++ Sources/octahe/router.swift
@@ -7,8 +7,70 @@
 
 import Foundation
 
+
+enum RouterError: Error {
+    case NoTargets(message: String)
+    case NotImplemented(message: String)
+}
+
 func CoreRouter(parsedOptions:Octahe.Options, function:String) throws {
-    let configFiles = try FileParser.buildRawConfigs(files: parsedOptions.configurationFiles)
+    let configFiles = try FileParser.buildRawConfigs(
+        files: parsedOptions.configurationFiles
+    )
+
     print("Running function:", function)
-    print(configFiles)
+    // Collect the ARG options, they could be used in TO or FROM
+
+    // TODO(cloudnull): The follow args need to be rendered and added to a CONSTANT
+    // Sourced from remote target at runtime
+    //    TARGETPLATFORM - platform of the build result. Eg linux/amd64, linux/arm/v7, windows/amd64.
+    //    TARGETOS - OS component of TARGETPLATFORM
+    //    TARGETARCH - architecture component of TARGETPLATFORM
+    //    TARGETVARIANT - variant component of TARGETPLATFORM\
+    // Sourced from local machine
+    //    BUILDPLATFORM - platform of the node performing the build.
+    //    BUILDOS - OS component of BUILDPLATFORM
+    //    BUILDARCH - architecture component of BUILDPLATFORM
+    //    BUILDVARIANT - variant component of BUILDPLATFORM
+
+    // TODO(cloudnull): ARG/ENV/LABEL format has three types: k=v, k v, k.
+    //                  Additionally multiple arguments can be set on a single line.
+    let octaheLabels = configFiles.filter{$0.key == "LABEL"}.map{$0.value}.reduce(into: [String: String]()) {
+        let argArray = $1.split(separator: "=", maxSplits: 1)
+        if let key = argArray.first, let value = argArray.last {
+            let cleanedKey = String(key).trimmingCharacters(in: .whitespacesAndNewlines)
+            let cleanedValue = String(value).trimmingCharacters(in: .whitespacesAndNewlines)
+            $0[cleanedKey] = cleanedValue
+        }
+    }
+
+    let octaheArgs = configFiles.filter{$0.key == "ARG" || $0.key == "ENV"}.map{$0.value}.reduce(into: [String: String]()) {
+        // This needs work, the ARG/ENV/LABEL format has three types: k=v, k v, k.
+        // Additionally multiple arguments can be set on a single line.
+        let argArray = $1.split(separator: "=", maxSplits: 1)
+        if let key = argArray.first, let value = argArray.last {
+            let cleanedKey = String(key).trimmingCharacters(in: .whitespacesAndNewlines)
+            let cleanedValue = String(value).trimmingCharacters(in: .whitespacesAndNewlines)
+            $0[cleanedKey] = cleanedValue
+        }
+    }
+
+    // Filter FROM options to send for introspection to return additional config from a container registry.
+    let octaheFrom = configFiles.filter{$0.key == "FROM"}.map{$0.value}
+    if octaheFrom.count > 0 {
+        throw(RouterError.NotImplemented(message: "This is where introspection will be queued..."))
+    }
+
+    // Filter all FROM options, they're not used any longer.
+    let octaheVerbs = configFiles.filter{$0.key != "FROM"}
+
+    // filter all TARGETS.
+    let targets = octaheVerbs.filter{$0.key == "TO"}
+    if targets.count < 1 {
+        throw(RouterError.NoTargets(message: "No Targets provided."))
+    }
+
+    // Return only a valid config.
+    let octaheConfig = octaheVerbs.filter{$0.key != "TO"}
+    print("Successfully deployed.")
 }
