f1d80808cca03cb8361cddbcc2a291d7b401873c
Kevin Carter <kecarter@redhat.com>
2020-07-12 23:31:26 -0500

    Improve comment parser and add functional tests for it
    This change will ensure Octahe is not matching on comments that are inline
    code. To ensure this is working, both an inline comment, and an in-code "#"
    have been used ensuring that it will be exercised on all program executions.
    
    Signed-off-by: Kevin Carter <kecarter@redhat.com>

--- Sources/octahe/parsers/containerFile.swift
+++ Sources/octahe/parsers/containerFile.swift
@@ -26,7 +26,7 @@ class FileParser {
     init() {}
 
     func trimLine(line: String) -> String {
-        let trimmed = line.replacingOccurrences(of: "#[^!].*", with: "", options: [.regularExpression])
+        let trimmed = line.replacingOccurrences(of: "(^#[^!*].*|\\s{1}#[^!*].*)", with: "", options: [.regularExpression])
         var trimmedLine = trimmed.strip
         if trimmedLine.hasSuffix(" \\") {
             trimmedLine = trimmedLine.replacingOccurrences(of: "\\", with: "")
